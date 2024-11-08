7c227c9f87f2b23a6e310ea4999c79c8ce8ffea3
navyaswetha68 <navyaswetha1821@gmail.com>
2024-11-08 11:50:52 +0530

    Add files via upload

---
+++ Day1/Employee.java
@@ -0,0 +1,15 @@
+class Employee
+{
+public static void main(String args[])
+{
+int eno=101;
+String ename="KARUN";
+float esal=345.34F;
+{
+System.out.println("the given employee number is:"+eno);
+System.out.println("the given employee name is:"+ename);
+System.out.println("the given employee salary is:"+esal);
+}
+}
+}
+
