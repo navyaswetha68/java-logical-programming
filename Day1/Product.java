2e2ef0c10acc023a590c204c5cd5e2dcd584e1d4
navyaswetha68 <navyaswetha1821@gmail.com>
2024-11-08 12:18:36 +0530

    Create Product.java

---
+++ Day1/Product.java
@@ -0,0 +1,12 @@
+class Product
+{
+public static void main(String args[])
+{
+int number=1234;
+String name="ice cream";
+float cost=10.5f;
+System.out.println("enter the product number:"+number);
+System.out.println("enter the product name:"+name);
+System.out.println("enter the product cost:"+cost);
+}
+} 
